d0453f3ea76b979a6447c84390d29c39ac8f4149
Sewmini Jayaweera <sewmi001@gmail.com>
2017-09-11 12:57:43 +0530

    first script on 'echo' command.

---
+++ first.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Different behaviors of 'echo'
+
+# Pass 2 arguements to echo
+echo Hello World
+
+# Pass single argument to echo
+echo "Hello	World!"
+
+# print  all in the current directory and hello.
+echo * hello
+
+# Need to Learn more on ``
+echo `hello` World
